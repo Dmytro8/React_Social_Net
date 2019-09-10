4d6a796168e0a954cfb3a2ff223d88b7067eb561
Dmytro <dmytro@Admins-Mac-mini.local>
2019-09-10 16:18:29 +0200

    little changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import logo from "./static/images/logo.png";
+import contentImage from "./static/images/content-image.jpg";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header>
+      <header className="header">
         <img src={logo} alt="logo img" />
       </header>
-      <nav>
+      <nav className="nav">
         <div>
           <a href="#profile">Profile</a>
         </div>
         <div>
           <a href="#messages">Messages</a>
         </div>
+        <div>
+          <a href="#messages">News</a>
+        </div>
+        <div>
+          <a href="#messages">Music</a>
+        </div>
+        <div>
+          <a href="#messages">Settings</a>
+        </div>
       </nav>
-      <main>Main content</main>
+      <main className="content">
+        <img src={contentImage} alt="content" />
+      </main>
+      <aside className="conversations">
+        <ul>
+          <li>Friend number 1</li>
+          <li>Friend number 2</li>
+          <li>Friend number 3</li>
+          <li>Friend number 4</li>
+          <li>Friend number 5</li>
+          <li>Friend number 6</li>
+        </ul>
+      </aside>
     </div>
   );
 }
